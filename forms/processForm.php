4f405d9c29edd2b6c1aeb91587a6d052b75ec236
Amogelang <tshiamomalebye3310@gmail.com>
2023-03-27 02:17:15 -0700

    hello

---
+++ forms/processForm.php
@@ -0,0 +1,12 @@
+<?php
+$recipient = "example@example.com"; // email address where the form data will be sent
+$name = $_POST['name']; // retrieve the user's name from the form data
+$email = $_POST['email']; // retrieve the user's email from the form data
+
+$msg = "Name: $name\nEmail: $email"; // create the message to be sent in the email
+
+// use the PHP mail function to send the email
+mail($recipient, 'New Form Submission', $msg);
+
+echo "Thank you for submitting the form!"; // output a thank you message to the user
+?>
